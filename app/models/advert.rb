4b348d0b4a0c0a9698bf6da8aa7e9a5497f7822c
gabriel roux <rouxgab81@gmail.com>
2022-08-11 14:30:34 -0400

    controller view done

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -1,3 +1,15 @@
 class Advert < ApplicationRecord
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
+
   belongs_to :user
+  # has_many :messages, dependent: :destroy
+  # has_many_attached :photos
+
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :type_ad, presence: true, inclusion:  { in: ["Lost", "Find", "See"], 
+    message: "%{value} is not a valid type" }
+  validates :address, presence: true
+  validates :content, presence: true
+
 end
