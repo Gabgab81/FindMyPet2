cc20bb5f9d46fa71c70b5ef67535df9d7008c98c
gabriel roux <rouxgab81@gmail.com>
2022-08-18 12:14:10 -0400

    user modification + show + advert done"

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :user_adverts]
+
+
+    def show
+
+    end
+
+    def user_adverts
+        # @adverts = Advert.where(user_id: @user)
+        @adverts = @user.adverts
+    end
+
+    def set_user
+        # @user = User.find(params[:id])
+        @user = User.find(current_user[:id])
+    end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   has_many :adverts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 3 }
+
 end
