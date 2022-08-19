dc6981e1e2c5e71c8dc3c724ab1308c073481343
gabriel roux <rouxgab81@gmail.com>
2022-08-19 08:50:23 -0400

    model conversation done

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,6 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :conversations, only: [:index, :show, :create]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_18_143839) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_19_124330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_143839) do
     t.datetime "updated_at", null: false
     t.index ["advert_id"], name: "index_comments_on_advert_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
