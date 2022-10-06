bdb6943768eb812748ce7d9651a78aa7c3e82502
gabriel roux <rouxgab81@gmail.com>
2022-10-06 11:44:03 -0400

    add picture to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :UserConversations, dependent: :destroy
+  has_one_attached :photo
 
   has_many :conversations, through: :UserConversations
 
