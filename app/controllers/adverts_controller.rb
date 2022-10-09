ef7aa4509f169f45b0af46217934207565427017
gabriel roux <rouxgab81@gmail.com>
2022-10-09 14:51:10 -0400

    can see index and show adv without login

--- app/controllers/adverts_controller.rb
+++ app/controllers/adverts_controller.rb
@@ -1,4 +1,5 @@
 class AdvertsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [ :index, :show ]
     before_action :set_advert, only: [:show, :edit, :update, :destroy]
 
     def new
