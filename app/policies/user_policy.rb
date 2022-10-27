e261b2047f08bd48a6a05166735f7456f565667a
gabriel roux <rouxgab81@gmail.com>
2022-10-27 15:24:26 -0400

    user pundit done + fix info window

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,17 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def show?
+    record == user
+  end
+
+  def user_adverts?
+    record == user
+  end
+
+end
